cab101b7a76343b1270e2801ddfabf0bc71ac398
jordan-bonecutter <jordan-bonecutter@users.noreply.github.com>
2022-04-03 17:21:20 +0000

    Linter found style changes

--- examples/perf.go
+++ examples/perf.go
@@ -18,152 +18,151 @@
 package main
 
 import (
-  "bytes"
-  _ "github.com/jordan-bonecutter/purplecrayon/svg"
-  pc "github.com/jordan-bonecutter/purplecrayon"
-  svgo "github.com/ajstarks/svgo"
-  "time"
-  "fmt"
-  "runtime"
-  "io"
-  "os"
+	"bytes"
+	"fmt"
+	svgo "github.com/ajstarks/svgo"
+	pc "github.com/jordan-bonecutter/purplecrayon"
+	_ "github.com/jordan-bonecutter/purplecrayon/svg"
+	"io"
+	"os"
+	"runtime"
+	"time"
 )
 
 const (
-  WARMUP_RUNS = 10
-  NUM_RECTS = 1000
-  NUM_GRADIENTS = 100
-  NUM_ELLIPSES = 100
-  NUM_PATHS = 1000
-  PATH_LEN = 100
-  CANV_WIDTH = 1920
-  CANV_HEIGHT = 1080
+	WARMUP_RUNS   = 10
+	NUM_RECTS     = 1000
+	NUM_GRADIENTS = 100
+	NUM_ELLIPSES  = 100
+	NUM_PATHS     = 1000
+	PATH_LEN      = 100
+	CANV_WIDTH    = 1920
+	CANV_HEIGHT   = 1080
 )
 
 func main() {
-  // Do a few warmup runs
-  for i := 0; i < WARMUP_RUNS; i++ {
-    perf(drawSVGO)
-    perf(drawPC)
-  }
+	// Do a few warmup runs
+	for i := 0; i < WARMUP_RUNS; i++ {
+		perf(drawSVGO)
+		perf(drawPC)
+	}
 
-  // The actual run!
-  svgoTime, svgoBytes := perf(drawSVGO)
-  pcTime, pcBytes := perf(drawPC)
+	// The actual run!
+	svgoTime, svgoBytes := perf(drawSVGO)
+	pcTime, pcBytes := perf(drawPC)
 
-  fmt.Printf("Took %v to draw in purplecrayon with %d bytes\n", pcTime, pcBytes.Len())
-  fmt.Printf("Took %v to draw in svgo with %d bytes\n", svgoTime, svgoBytes.Len())
+	fmt.Printf("Took %v to draw in purplecrayon with %d bytes\n", pcTime, pcBytes.Len())
+	fmt.Printf("Took %v to draw in svgo with %d bytes\n", svgoTime, svgoBytes.Len())
 
-  fi, _ := os.Create("pc.svg")
-  io.Copy(fi, pcBytes)
-  fi.Close()
+	fi, _ := os.Create("pc.svg")
+	io.Copy(fi, pcBytes)
+	fi.Close()
 
-  fi, _ = os.Create("svgo.svg")
-  io.Copy(fi, svgoBytes)
-  fi.Close()
+	fi, _ = os.Create("svgo.svg")
+	io.Copy(fi, svgoBytes)
+	fi.Close()
 }
 
 func perf(draw func(*bytes.Buffer)) (duration time.Duration, drawing *bytes.Buffer) {
-  buf := new(bytes.Buffer)
-  runtime.GC()
+	buf := new(bytes.Buffer)
+	runtime.GC()
 
-  start := time.Now()
-  draw(buf)
-  end := time.Now()
+	start := time.Now()
+	draw(buf)
+	end := time.Now()
 
-  return end.Sub(start), buf
+	return end.Sub(start), buf
 }
 
 func svgoRGB(r, g, b uint8) string {
-  return fmt.Sprintf("rgb(%d, %d, %d)", r, g, b)
+	return fmt.Sprintf("rgb(%d, %d, %d)", r, g, b)
 }
 
 func svgoFillStyle(color string) string {
-  return fmt.Sprintf(`fill="%s"`, color)
+	return fmt.Sprintf(`fill="%s"`, color)
 }
 
 func svgoStrokeStyle(color string) string {
-  return fmt.Sprintf(`stroke="%s"`, color)
+	return fmt.Sprintf(`stroke="%s"`, color)
 }
 
 func drawSVGO(buf *bytes.Buffer) {
-  canv := svgo.New(buf)
-  canv.Start(1920, 1080)
-  var idCount uint64
-  nextId := func() string {
-    idCount++
-    return fmt.Sprintf("obj-%d", idCount)
-  }
-  
-  for nr := 0; nr < NUM_RECTS; nr++ {
-    canv.Rect(20 + (nr/2), 30 + (nr/2), 100, 200, svgoFillStyle(svgoRGB(200, 100, 50+uint8(nr/10))))
-  }
+	canv := svgo.New(buf)
+	canv.Start(1920, 1080)
+	var idCount uint64
+	nextId := func() string {
+		idCount++
+		return fmt.Sprintf("obj-%d", idCount)
+	}
 
-  for ng := 0; ng < NUM_GRADIENTS; ng++ {
-    canv.LinearGradient(nextId(), 0, 0, 100, 100, []svgo.Offcolor{
-      svgo.Offcolor{
-        Offset: 10,
-        Color: svgoRGB(200, 100, 50),
-        Opacity: 1.0,
-      }, svgo.Offcolor{
-        Offset: 90,
-        Color: svgoRGB(50, 100, 200),
-        Opacity: 1.0,
-      },
-    })
-  }
+	for nr := 0; nr < NUM_RECTS; nr++ {
+		canv.Rect(20+(nr/2), 30+(nr/2), 100, 200, svgoFillStyle(svgoRGB(200, 100, 50+uint8(nr/10))))
+	}
 
-  for ne := 0; ne < NUM_ELLIPSES; ne++ {
-    canv.Ellipse(300, 400, 100, 50, svgoFillStyle("none"))
-  }
+	for ng := 0; ng < NUM_GRADIENTS; ng++ {
+		canv.LinearGradient(nextId(), 0, 0, 100, 100, []svgo.Offcolor{
+			svgo.Offcolor{
+				Offset:  10,
+				Color:   svgoRGB(200, 100, 50),
+				Opacity: 1.0,
+			}, svgo.Offcolor{
+				Offset:  90,
+				Color:   svgoRGB(50, 100, 200),
+				Opacity: 1.0,
+			},
+		})
+	}
 
-  for np := 0; np < NUM_PATHS; np++ {
-    pathD := "M 100 200"
-    for pi := 0; pi < PATH_LEN; pi++ {
-      pathD += fmt.Sprintf(" q %.0f %.0f %.0f %.0f", 25.0, 25.0, 33.0, 33.0)
-    }
-    pathD += " z"
-    canv.Path(pathD, svgoStrokeStyle(svgoRGB(200, 50, 32)))
-  }
-  canv.End()
+	for ne := 0; ne < NUM_ELLIPSES; ne++ {
+		canv.Ellipse(300, 400, 100, 50, svgoFillStyle("none"))
+	}
+
+	for np := 0; np < NUM_PATHS; np++ {
+		pathD := "M 100 200"
+		for pi := 0; pi < PATH_LEN; pi++ {
+			pathD += fmt.Sprintf(" q %.0f %.0f %.0f %.0f", 25.0, 25.0, 33.0, 33.0)
+		}
+		pathD += " z"
+		canv.Path(pathD, svgoStrokeStyle(svgoRGB(200, 50, 32)))
+	}
+	canv.End()
 }
 
 func drawPC(buf *bytes.Buffer) {
-  canv, _ := pc.NewCanvas("svg", CANV_WIDTH, CANV_HEIGHT, buf)
-  
-  for nr := 0; nr < NUM_RECTS; nr++ {
-    rect := canv.Rect().TopLeft(pc.Point{20 + float64(nr/2), 30 + float64(nr/2)}).Width(100).Height(200)
-    rect.FillRGB(pc.RGB{200, 100, 50+uint8(nr/10)})
-    rect.Close()
-  }
+	canv, _ := pc.NewCanvas("svg", CANV_WIDTH, CANV_HEIGHT, buf)
 
-  for ng := 0; ng < NUM_GRADIENTS; ng++ {
-    grad := canv.LinearGradient()
-    grad.SetLine(pc.Point{0, 0}, pc.Point{1, 1})
-    stops := grad.GradientStops()
-    stops.Stop().RGB(pc.RGB{200, 100, 50}).Position(0.1).Finish()
-    stops.Stop().RGB(pc.RGB{50, 100, 200}).Position(0.9).Finish()
-    stops.Finish()
-    grad.Close()
-  }
+	for nr := 0; nr < NUM_RECTS; nr++ {
+		rect := canv.Rect().TopLeft(pc.Point{20 + float64(nr/2), 30 + float64(nr/2)}).Width(100).Height(200)
+		rect.FillRGB(pc.RGB{200, 100, 50 + uint8(nr/10)})
+		rect.Close()
+	}
 
-  for ne := 0; ne < NUM_ELLIPSES; ne++ {
-    ellipse := canv.Ellipse().Center(pc.Point{300, 400}).Radii(pc.Point{100, 50})
-    ellipse.FillTransparent()
-    ellipse.Close()
-  }
+	for ng := 0; ng < NUM_GRADIENTS; ng++ {
+		grad := canv.LinearGradient()
+		grad.SetLine(pc.Point{0, 0}, pc.Point{1, 1})
+		stops := grad.GradientStops()
+		stops.Stop().RGB(pc.RGB{200, 100, 50}).Position(0.1).Finish()
+		stops.Stop().RGB(pc.RGB{50, 100, 200}).Position(0.9).Finish()
+		stops.Finish()
+		grad.Close()
+	}
 
-  for np := 0; np < NUM_PATHS; np++ {
-    path := canv.Path()
-    cursor := path.Cursor().MoveTo(pc.Point{100, 200})
-    for pi := 0; pi < PATH_LEN; pi++ {
-      cursor.QuadToRel(pc.Point{25, 25}, pc.Point{33, 33})
-    }
-    cursor.Zip()
-    cursor.Finish()
-    path.StrokeRGB(pc.RGB{200, 50, 32})
-    path.Close()
-  }
-  canv.Close()
+	for ne := 0; ne < NUM_ELLIPSES; ne++ {
+		ellipse := canv.Ellipse().Center(pc.Point{300, 400}).Radii(pc.Point{100, 50})
+		ellipse.FillTransparent()
+		ellipse.Close()
+	}
+
+	for np := 0; np < NUM_PATHS; np++ {
+		path := canv.Path()
+		cursor := path.Cursor().MoveTo(pc.Point{100, 200})
+		for pi := 0; pi < PATH_LEN; pi++ {
+			cursor.QuadToRel(pc.Point{25, 25}, pc.Point{33, 33})
+		}
+		cursor.Zip()
+		cursor.Finish()
+		path.StrokeRGB(pc.RGB{200, 50, 32})
+		path.Close()
+	}
+	canv.Close()
 }
-
